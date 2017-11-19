c45e187b13ad1f9570d8cdd6ce749439fe412bdc
jiangsongzhan <jiangsongzhan94@163.com>
2017-11-19 23:32:39 +0800

    sunday

--- taotao-manager/taotao-manager-web/src/test/java/com/taotao/controller/TestCase.java
+++ taotao-manager/taotao-manager-web/src/test/java/com/taotao/controller/TestCase.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.mapper.TbItemMapper;
@@ -17,8 +18,8 @@ public class TestCase {
 	@Test
 	public void testPageHelper(){
 		//创建一个Spring容器
-		ApplicationContext application = 
-				new ClassPathXmlApplicationContext("classpath:spring/applicationContext-*.xml");
+		ApplicationContext application = new ClassPathXmlApplicationContext
+				("classpath:spring/applicationContext-*.xml");
 		//从spring容器中获取Mapper代理对象
 		TbItemMapper mapper = application.getBean(TbItemMapper.class);
 		TbItemExample example = new TbItemExample();
@@ -26,6 +27,10 @@ public class TestCase {
 		List<TbItem> list = mapper.selectByExample(example);
 		for (TbItem item:list) {
 			System.out.println(item.getTitle());
+		}
+		if(list instanceof Page){
+			System.out.println("page");
+			System.out.println("size:"+list.size());
 		}
 		long total = new PageInfo<TbItem>(list).getTotal();
 		System.out.println("共有商品"+total+"件");
